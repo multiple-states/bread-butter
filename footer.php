8606b51dd8df0d0c4c8570128e0cf6a1f273255e
Will McLean <will@mintcreative.com>
2013-12-10 16:16:16 +0000

    Add jquery ui.

--- footer.php
+++ footer.php
@@ -8,6 +8,7 @@
 
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
 <script src="<?php echo $site_url ?>/js/custom.js"></script>
 
 </body>
