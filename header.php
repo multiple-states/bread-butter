91a4b8a064c40399736035ed0ee1c0885e81bb5b
Will McLean <will@mintcreative.com>
2013-12-16 16:49:36 +0000

    Add mobile-web-app-capable meta to head

--- header.php
+++ header.php
@@ -1,23 +1,28 @@
  <!DOCTYPE html> 
 
 <html>
-<head>
-<?php include('functions.php')?>
-<title>Title</title>
 
-<meta name="viewport" content="width=device-width, initial-scale=1">
-
-<link href="<?php echo $site_url ?>/css/style.css" rel="stylesheet" type="text/css" charset="utf-8">
-
-</head>
-<body>
+	<head>
+	<?php include('functions.php')?>
+	<title>Title</title>
 	
-	<header>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<meta name="mobile-web-app-capable" content="yes">
+	<link rel="shortcut icon" sizes="196x196" href="<?php echo $site_url ?>/your-icon.png">
+	<link rel="apple-touch-icon" sizes="196x196" href="<?php echo $site_url ?>/your-icon.png">
+	
+	<link href="<?php echo $site_url ?>/css/style.css" rel="stylesheet" type="text/css" charset="utf-8">
+
+	</head>
+	<body>
 		
-		<div>Header</div>
-		
-		<div><a href="<?php echo $site_url ?>">Index</a> <a href="<?php echo $site_url ?>/page">page 1</a></div>
-		
-	</header>
+		<header>
+			
+			<div>Header</div>
+			
+			<div><a href="<?php echo $site_url ?>">Index</a> <a href="<?php echo $site_url ?>/page">page 1</a></div>
+			
+		</header>
 
 	
