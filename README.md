65182a38cf9f1dbf1b338ef5bcee053348bfbac5
Will McLean <willmcl@gmail.com>
2014-04-28 10:38:56 +0100

    Update documentation

--- README.md
+++ README.md
@@ -3,7 +3,13 @@ php-starter-template
 
 A php starter template.
 
-Uses less.
+Stlying:
+
+Uses less and tries to follow the SMACCS methodology documneted <a href="http://smacss.com/">here</a>.
+
+A default structure 
+
+Bootstrap:
 
 Uses bootstrap for the grid: http://getbootstrap.com/
 Bootstrap is added as a submodule within src>less>bootstrap
@@ -15,9 +21,12 @@ when cloning this for the first time.
 
 Because I don't want all of bootstrap just the grid (I want my designs and builds to have a personality of their own) I have built my own custom version by importing only the less files I want at the top of my style.less file. 
 
+The Grunt build setup:
+
 Uses grunt to compile the less files, minify the css and js and compile into the 'app' folder. The gruntfile.js has pretty decent comments.
 You will have to install these dependencies (as well as node package manager and grunt) in order to run the grunt commands:
 
+grunt-contrib-copy
 grunt-contrib-uglify
 grunt-contrib-jshint
 grunt-contrib-less
@@ -27,6 +36,22 @@ grunt-contrib-watch
 
 Use "grunt watch" when developing as this will automatically compile your less. There is a livereload option set in the watch task but you need to install an extension on your browser for it to work. 
 <a href="http://feedback.livereload.com/knowledgebase/articles/86242-how-do-i-install-and-use-the-browser-extensions-">Get them here.</a>
+
+Snippets:
+
+A snippets system has been started to include more common elements of code (like menus). 
+
+The styles for can be found in src>less>snippets
+The mark-up can be found in src>snippets
+The javascript can be found in src>js>snippets
+
+When adding a snippet you will need to
+1. Import the default styles into your modules.less file
+2. Include the php file within your markup
+3. Include the .js file (still working on this)
+4. Add your file to the copy task in gruntfile.js and run the default grunt task. (This will ensure your snippet will be included in your production)
+
+Other useful stuff:
 
 Set your local and remote urls in functions.php
 
--- app/pattern.php
+++ app/pattern.php
@@ -1,18 +1,20 @@
 <?php include('header.php')?>
 
 	<div class="l-strip">
-		<div class="l-inner">
-			<div class="pattern-typography">
-				<h1><a href="#">Heading 1</a></h1>
-				<h2><a href="#">Heading 2</a></h2>
-				<h3><a href="#">Heading 3</a></h3>
-				<h4><a href="#">Heading 4</a></h4>
-				<h5><a href="#">Heading 5</a></h5>
-				<h6><a href="#">Heading 6</a></h6>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod, est sit amet lobortis pulvinar, libero magna congue ligula, sit amet dignissim nisl tellus non tortor. Donec nisl purus, convallis quis erat ac, posuere blandit nunc. Vivamus ornare purus ac adipiscing fermentum. <a href="#">Here is a link within a paragraph</a> <span class="p-highlight">Try hovering over it!</span>. Vestibulum feugiat pretium lectus ut fermentum. Integer quis felis nec nisi dictum faucibus. Nulla sed malesuada enim, consequat euismod tortor. Sed tempus, risus eu tempor placerat, felis mauris placerat ipsum, eget auctor magna sapien eu nisl. Nam adipiscing pellentesque semper.
-
-Nullam rutrum nunc mauris. Proin eu gravida turpis. Phasellus congue nisi a pellentesque porttitor. Duis facilisis ullamcorper nunc, sed pellentesque quam. Quisque tempor at lorem in faucibus. Donec elementum accumsan ante, tincidunt pretium lorem tincidunt et. Nunc in lorem lacinia, eleifend risus quis, sodales ante. Maecenas et ullamcorper elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean convallis, nisl eget aliquam fermentum, neque neque consequat velit, ut tincidunt enim arcu a erat. Ut ut nunc nulla. Pellentesque sit amet dictum risus, a elementum dolor.</p>
-				<a class="button" href="#">A button</a>
+		<div class="l-holder">
+			<div class="l-inner">
+				<div class="pattern-typography">
+					<h1><a href="#">Heading 1</a></h1>
+					<h2><a href="#">Heading 2</a></h2>
+					<h3><a href="#">Heading 3</a></h3>
+					<h4><a href="#">Heading 4</a></h4>
+					<h5><a href="#">Heading 5</a></h5>
+					<h6><a href="#">Heading 6</a></h6>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In euismod, est sit amet lobortis pulvinar, libero magna congue ligula, sit amet dignissim nisl tellus non tortor. Donec nisl purus, convallis quis erat ac, posuere blandit nunc. Vivamus ornare purus ac adipiscing fermentum. <a href="#">Here is a link within a paragraph</a> <span class="p-highlight">Try hovering over it!</span>. Vestibulum feugiat pretium lectus ut fermentum. Integer quis felis nec nisi dictum faucibus. Nulla sed malesuada enim, consequat euismod tortor. Sed tempus, risus eu tempor placerat, felis mauris placerat ipsum, eget auctor magna sapien eu nisl. Nam adipiscing pellentesque semper.
+	
+	Nullam rutrum nunc mauris. Proin eu gravida turpis. Phasellus congue nisi a pellentesque porttitor. Duis facilisis ullamcorper nunc, sed pellentesque quam. Quisque tempor at lorem in faucibus. Donec elementum accumsan ante, tincidunt pretium lorem tincidunt et. Nunc in lorem lacinia, eleifend risus quis, sodales ante. Maecenas et ullamcorper elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean convallis, nisl eget aliquam fermentum, neque neque consequat velit, ut tincidunt enim arcu a erat. Ut ut nunc nulla. Pellentesque sit amet dictum risus, a elementum dolor.</p>
+					<a class="button" href="#">A button</a>
+				</div>
 			</div>
 		</div>
 	</div>
