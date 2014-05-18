7023a9edf1adadccc408c807ee17620fde19002e
Will McLean <willmcl@gmail.com>
2014-05-18 22:08:03 +0100

    Convert tabs to two spaces across all

--- app/snippets/fly-out-menu.php
+++ app/snippets/fly-out-menu.php
@@ -1,24 +1,24 @@
 <!-- Relies on: menu-open-btn -->
 
 <div class="m-nav-holder">
-	
-	<div class="m-first-nav">
-		<ul>
-			<li><a href="#">Menu item 1</a></li>
-			<li>
-				<a href="#">Menu item 2</a>
-				<ul>
-					<li><a href="#">Sub-menu item 1</a></li>
-					<li><a href="#">Sub-menu item 2</a></li>
-					<li><a href="#">Sub-menu item 3</a></li>
-				</ul>
-			</li>
-			<li><a href="#">Menu item 3</a></li>
-			<li><a href="#">Menu item 4</a></li>
-			<li><a href="#">Menu item 5</a></li>
-			<li><a href="#">Menu item 6</a></li>
-			<li><a href="#">Menu item 7</a></li>
-		</ul>
-	</div> <!-- end m-first-nav -->
-			
+  
+  <div class="m-first-nav">
+    <ul>
+      <li><a href="#">Menu item 1</a></li>
+      <li>
+        <a href="#">Menu item 2</a>
+        <ul>
+          <li><a href="#">Sub-menu item 1</a></li>
+          <li><a href="#">Sub-menu item 2</a></li>
+          <li><a href="#">Sub-menu item 3</a></li>
+        </ul>
+      </li>
+      <li><a href="#">Menu item 3</a></li>
+      <li><a href="#">Menu item 4</a></li>
+      <li><a href="#">Menu item 5</a></li>
+      <li><a href="#">Menu item 6</a></li>
+      <li><a href="#">Menu item 7</a></li>
+    </ul>
+  </div> <!-- end m-first-nav -->
+      
 </div><!-- end nav-holder -->
