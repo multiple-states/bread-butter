3c1b3f97dffda0d8a744d7a0670ba043fcbc027b
Will McLean <willmcl@gmail.com>
2014-05-26 10:08:12 +0100

    Update menu include to have links to default pages

--- src/includes/menu.php
+++ src/includes/menu.php
@@ -1,14 +1,14 @@
 <ul>
-  <li><a href="#">Menu item 1</a></li>
+  <li><a href="<?php echo $site_url ?>">Index</a></li>
+  <li><a href="<?php echo $site_url ?>/pattern">Pattern</a></li>
   <li>
-    <a href="#">Menu item 2</a>
+    <a href="#">Menu item 3</a>
     <ul>
       <li><a href="#">Sub-menu item 1</a></li>
       <li><a href="#">Sub-menu item 2</a></li>
       <li><a href="#">Sub-menu item 3</a></li>
     </ul>
   </li>
-  <li><a href="#">Menu item 3</a></li>
   <li><a href="#">Menu item 4</a></li>
   <li><a href="#">Menu item 5</a></li>
   <li><a href="#">Menu item 6</a></li>
