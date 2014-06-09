f81713e8bf445fd8b86ae14bfe5a87e9dc6db92c
Will McLean <willmcl@gmail.com>
2014-06-09 14:08:19 +0100

    Resolve issue #19. Make functions.php more user friendly.

--- app/functions.php
+++ app/functions.php
@@ -1,13 +1,32 @@
 <?php
-// check IP to set site root 
-// source: http://stackoverflow.com/a/7614272/1202344
-$host = gethostname();
-$ip = gethostbyname($host);
-if ($ip == '192.168.1.38') 
-{
-  $site_url = "http://localhost/00-php-starter-template/app/";
-} elseif ($ip == '192.168.0.4') 
-{
-  $site_url = "http://localhost:8888/php-starter-template/app/";
-}
+
+  // A function to check an IP address and then set the site root variable to the relevant url
+  
+  // source: http://stackoverflow.com/a/7614272/1202344
+  
+  $host = gethostname();
+  $ip = gethostbyname($host);
+  
+  // Test for the first contributers ip address
+  if ($ip == '192.168.1.38') 
+  {
+    // Then they can set their site root variable
+    $site_url = "http://localhost/00-php-starter-template/app/";
+    
+  } 
+  
+  // Test for the second contributers ip address
+  elseif ($ip == '192.168.0.4') 
+  {
+    // Then they can set their site root variable
+    $site_url = "http://localhost:8888/php-starter-template/app/";
+  
+  } 
+  
+  // Then for all else we can set the staging url as the site root variable
+  else 
+  {
+    $site_url = "http://your-staging-url.com";
+  }
+
 ?>
