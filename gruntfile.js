0272280e8d2b8c3488d51378841b58dca5b7f035
k33711647 <mail@kevin-beck.co.uk>
2014-09-23 11:16:45 +0100

    add grunt task to create fallback px for all rem

--- gruntfile.js
+++ gruntfile.js
@@ -38,7 +38,7 @@ module.exports = function(grunt) { // Grunt wrapper - Do grunt-related things in
         }
       },
 
-      modules: {
+      modules: {        
         // This copies the modules from the src/modules folder to the app/modules folder
         files: [
           {
@@ -75,7 +75,7 @@ module.exports = function(grunt) { // Grunt wrapper - Do grunt-related things in
       },
       
       fontcopy:{ 
-      //This task will find the relevant font files in the src/fonts folder and move them to the app/fonts folder.
+      // This task will find the relevant font files in the src/fonts folder and move them to the app/fonts folder.
         files: [
           {
             expand: true,
@@ -88,6 +88,7 @@ module.exports = function(grunt) { // Grunt wrapper - Do grunt-related things in
     },
     
     uglify: {
+
       // The base uglify task
       base: {
         options: {
@@ -100,9 +101,10 @@ module.exports = function(grunt) { // Grunt wrapper - Do grunt-related things in
         '<%= path %>/js/modernizer.min.js': ['src/js/modernizer.js']
         }
       },
+
       // The snippets uglify task for snippets with js files.
       // All snippets are added you just need to uncomment the ones you have included in your markup.
-      snippets: {
+      snippets: {        
         options: {
           mangle: false,
           // If you want to add banners to your minified css uncomment the below. This currently is commented out in order to clean up commits in development.
@@ -112,22 +114,23 @@ module.exports = function(grunt) { // Grunt wrapper - Do grunt-related things in
         // '<%= path %>/js/google-map.min.js': ['src/snippets/google-map.js'],
         }
       }
-
     },
     
     jshint: {
+
       // The jshint task. 
       files: [
       'src/js/custom.js', // Just watch the src js file not the minified one
 
-      //The snippets
+      // The snippets
       // All snippets are listed below you just need to uncomment the ones you have included in order to have them tested when running "grunt test"
 
-      //'src/snippets/google-map.js',
+      // 'src/snippets/google-map.js',
       ] 
     },
     
     less: { 
+
       // Configure the less task
       // The compilation of our css goes through three stages:
       // 1. We use this less task to compile the less to css in the src folder
@@ -149,20 +152,19 @@ module.exports = function(grunt) { // Grunt wrapper - Do grunt-related things in
     
     autoprefixer: { 
 
-      // 2. We use the auto-prefixer task to put prefixes for various browsers into our css file and move it to the app folder
+      // We use the auto-prefixer task to put prefixes for various browsers into our css file and move it to the app folder
       options: {
         browsers: ['last 3 versions'] // More options available for this see https://github.com/ai/autoprefixer#browsers
       },
       files: {
         src: 'src/css/style.css',
         dest: '<%= path %>/style.css' // Put the autoprefixed version right back over its original
-      },
-
-    },
+      }
+    },    
     
-    cssmin: { 
+    cssmin: {
 
-      // 3. We use the cssmin task to minify that css file into a new file with the .min.css extension
+      // We use the cssmin task to minify that css file into a new file with the .min.css extension
       minify: {
         options: {
           // If you want to add banners to your minified css uncomment the below. This currently is commented out in order to clean up commits in development.
@@ -173,7 +175,7 @@ module.exports = function(grunt) { // Grunt wrapper - Do grunt-related things in
         }
       },
 
-      //This task will find the stylesheet in the webkit folder, minify it and move it to the app folder
+      // This task will find the stylesheet in the webkit folder, minify it and move it to the app folder
       fontmin: { 
         files: [
           {
@@ -185,7 +187,18 @@ module.exports = function(grunt) { // Grunt wrapper - Do grunt-related things in
           }
         ]
       }
+    },
 
+    pixrem: {
+
+      // We run pixrem on that file to create fallback for our use of rem units. We rewrite the result over itself.
+      options: {
+        rootvalue: '62.5%' // We set this so that the 1 rem unit equals 10px. Easier to understand.
+      },
+      dist: {
+        src: '<%= path %>/style.min.css',
+        dest: '<%= path %>/style.min.css'
+      }
     },
     
     watch: { 
@@ -223,6 +236,10 @@ module.exports = function(grunt) { // Grunt wrapper - Do grunt-related things in
         files: ['src/**/*.less'],
         tasks: ['less', 'autoprefixer', 'cssmin:minify'],
       },
+      css: {
+        files: ['src/**/*.less'],
+        tasks: ['pixrem'],
+      },
       livereload: {
         // Here we watch the files the watch task will change.
         // These files are then sent to the live reload server.
@@ -242,6 +259,7 @@ module.exports = function(grunt) { // Grunt wrapper - Do grunt-related things in
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-pixrem');
     
     // Register a test task for jshint. This can be run just by typing "grunt test" on the command line
     grunt.registerTask('test', ['jshint'])
@@ -253,6 +271,6 @@ module.exports = function(grunt) { // Grunt wrapper - Do grunt-related things in
     grunt.registerTask('fontload', ['copy:fontcopy', 'cssmin:fontmin'])
     
     // And register the default task. This can be run just by typing "grunt" on the command line. This should be done before production.
-    grunt.registerTask('default', ['copy:pages', 'copy:snippets', 'copy:modules', 'copy:includes', 'copy:images', 'uglify', 'less', 'autoprefixer', 'cssmin']);
+    grunt.registerTask('default', ['copy:pages', 'copy:snippets', 'copy:modules', 'copy:includes', 'copy:images', 'uglify', 'less', 'autoprefixer', 'cssmin', 'pixrem']);
     
 };
